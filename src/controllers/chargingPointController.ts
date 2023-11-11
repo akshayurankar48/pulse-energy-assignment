b5eda6515fe1b8560917c3b878b770581ee9a5ab
evilieswithin <evilieswithin>
2023-11-11 12:13:40 +0530

    add support for pagination and certain charge point id's

--- src/controllers/chargingPointController.ts
+++ src/controllers/chargingPointController.ts
@@ -9,8 +9,31 @@ export const getAllChargingData = async (
   res: express.Response
 ) => {
   try {
+    // Extract pagination parameters from the request query
+    const page = Number(req.query.page) || 0;
+    const dataPerPage = 10;
+
+    // Extract charge point IDs from the request query and split them
+    let chargePointIds: string[] = [];
+
+    if (Array.isArray(req.query.chargePointIds)) {
+      chargePointIds = req.query.chargePointIds
+        .map(String)
+        .flatMap((id) => id.split(','));
+    } else if (typeof req.query.chargePointIds === 'string') {
+      chargePointIds = req.query.chargePointIds.split(',');
+    }
+
+    // Create a filter object based on charge point IDs
+    const filter =
+      chargePointIds.length > 0
+        ? { charge_point_id: { $in: chargePointIds } }
+        : {};
+
     // Query MongoDB to get charging data based on the filter and pagination
-    const chargingData = await ChargingData.find({});
+    const chargingData = await ChargingData.find(filter)
+      .skip(page * dataPerPage)
+      .limit(dataPerPage);
 
     // Send a JSON response with the retrieved charging data
     res.status(200).json(chargingData);
