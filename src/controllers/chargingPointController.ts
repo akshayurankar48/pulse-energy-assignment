91a8fd153af59dc60ee47825dc1f4a3096dcbe58
evilieswithin <evilieswithin>
2023-11-11 12:10:44 +0530

    get data for all charging data

--- src/controllers/chargingPointController.ts
+++ src/controllers/chargingPointController.ts
@@ -7,7 +7,19 @@ import { ChargingData } from '../models/chargingDataModel';
 export const getAllChargingData = async (
   req: express.Request,
   res: express.Response
-) => {};
+) => {
+  try {
+    // Query MongoDB to get charging data based on the filter and pagination
+    const chargingData = await ChargingData.find({});
+
+    // Send a JSON response with the retrieved charging data
+    res.status(200).json(chargingData);
+  } catch (error) {
+    // Log and handle any errors that occur during the process
+    console.log(error);
+    return res.sendStatus(400);
+  }
+};
 
 // Controller function to get charging data by ID
 export const getChargingDataById = async (
