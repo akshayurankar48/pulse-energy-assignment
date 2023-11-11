dc85f285519d5c1b882d056c77db431bf72aa5f0
evilieswithin <evilieswithin>
2023-11-11 12:01:44 +0530

    create routes for charging data

---
+++ src/routes/chargingPointRoutes.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+// Create an instance of Express Router
+const router = express.Router();
+
+// Define and export a function to configure and return the charging point router
+export default (): express.Router => {
+  // Define route for getting all charging data
+  router.get('/charging-data', getAllChargingData);
+
+  // Define route for getting charging data by ID
+  router.get('/charging-data/:id', getChargingDataById);
+
+  // Return the configured router
+  return router;
+};
