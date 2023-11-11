561dc89d7c8a922a13602efffd6b03d36ea2ca75
evilieswithin <evilieswithin>
2023-11-11 11:28:16 +0530

    setup mongoose on the mqtt server

--- mqtt-server.ts
+++ mqtt-server.ts
@@ -3,9 +3,11 @@ require('dotenv').config();
 
 // Import MQTT-related modules and MongoDB-related modules
 import { MqttClient, connect } from 'mqtt';
+import mongoose from 'mongoose';
 
 // Get MQTT broker and MongoDB connection URLs from environment variables
 const BROKER_URL: string = process.env.BROKER_URL || '';
+const MONGO_URI: string = process.env.MONGO_URI || '';
 
 // Connect to the MQTT broker
 const mqttServer: MqttClient = connect(BROKER_URL);
@@ -15,6 +17,22 @@ mqttServer.on('connect', () => {
   // Subscribe to the 'pulseEnergy' topic
   mqttServer.subscribe('pulseEnergy');
   console.log('MQTT Server started successfully!');
+
+  // Set MongoDB options and connect to the database
+  mongoose.set('strictQuery', false);
+  mongoose.Promise = Promise;
+  mongoose.connect(MONGO_URI);
+
+  // Get the MongoDB connection
+  const db: mongoose.Connection = mongoose.connection;
+
+  // Event handler for MongoDB connection errors
+  db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+  // Event handler once MongoDB connection is open
+  db.once('open', () => {
+    console.log('Connected to MongoDB');
+  });
 });
 
 // Event handlers for MQTT server close, end, and error events
